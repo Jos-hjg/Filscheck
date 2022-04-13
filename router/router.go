56bf7cb15155939e69e539723d0485a26eb25da5
huangjg <huang_learn@163.com>
2022-04-13 18:33:35 +0800

    cors default

--- router/router.go
+++ router/router.go
@@ -8,14 +8,10 @@ import (
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
-	config := cors.DefaultConfig()
-	//config.AllowAllOrigins = true
-	config.AllowOrigins = []string{"http://localhost:63342"}
 
-	r.Use(cors.New(config))
+	r.Use(cors.Default())
 	//TODO: any router?
 	r.GET("/miner", controller.GetMinerInfo)
 	r.GET("/mining-data", controller.GetMiningData)
 	return r
 }
-
